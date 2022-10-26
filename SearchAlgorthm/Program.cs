fc85395d13f9b9ab823043ee67da322b27fe7368
Izalhadji31 <115075859+Izalhadji31@users.noreply.github.com>
2022-10-26 17:02:49 +0700

    menambahkan linearsearch

--- SearchAlgorthm/Program.cs
+++ SearchAlgorthm/Program.cs
@@ -43,5 +43,42 @@ namespace SearchAlgorthm
 
         public void BinarySearch()
         {
+            char ch;
+            do
+            {
+                //Accept the number to be searched
+                Console.Write("\nEnter element want you to search: ");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                //Apply binary search
+                int lowerbound = 0;
+                int upperbound = n - 1;
+
+                //Obtain the index of the middle elements
+                int mid = (lowerbound + upperbound) / 2;
+                int ctr = 1;
+
+                //loop to search for the elements in the array
+                while ((item != arr[mid]) && (lowerbound <= upperbound))
+                {
+                    if (item > arr[mid])
+                        lowerbound = mid + 1;
+                    else
+                        upperbound = mid - 1;
+                    mid = (lowerbound + upperbound) / 2;
+                    ctr++;
+                }
+                if (item < arr[mid])
+                    Console.WriteLine("\n" + item.ToString() + "found at position" + (mid + 1).ToString());
+                else
+                    Console.WriteLine("\n" + item.ToString() = "not found in the array");
+                Console.WriteLine("\nNumber of comprasion :" + ctr);
+
+                Console.Write("\nContinue search (y/n)");
+                ch = char.Parse(Console.ReadLine().ToUpper());
+            } while ((ch == 'y'));
+        }
+        public void LinearSearch()
+        {
 
         }
