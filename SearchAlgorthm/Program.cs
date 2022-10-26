e89e8d6d3beabd4edc46a3d0a63e9232e55fe1ab
Izalhadji31 <115075859+Izalhadji31@users.noreply.github.com>
2022-10-26 16:22:38 +0700

    menambahkan enter the number

--- SearchAlgorthm/Program.cs
+++ SearchAlgorthm/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace SearchAlgorthm
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        //Array to be searched
+        int[] arr = new int[20];
+        //Number of elements in the array
+        int n;
+        //Get the number of elements to store in the array
+        int i;
+
+
+        public  void input()
         {
+            while (true)
+        {
+            Console.WriteLine("Enter the number of elements in the array: ");
+            string s = Console.ReadLine();
+            n = Int32.Parse(s);
+            if((n > 0) && (n <= 20))
+               break;
+            else
+                Console.WriteLine("\nArray should have minimum 1 and maximum 20 elements. \n");
         }
     }
 }
