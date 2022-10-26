bc73ad2c6eafa9d98c123562a9c4ca0f9f4b0d3e
Izalhadji31 <115075859+Izalhadji31@users.noreply.github.com>
2022-10-26 16:37:01 +0700

    menambahkan binarysarch

--- SearchAlgorthm/Program.cs
+++ SearchAlgorthm/Program.cs
@@ -16,17 +16,32 @@ namespace SearchAlgorthm
         int i;
 
 
-        public  void input()
+        public void input()
         {
             while (true)
-        {
-            Console.WriteLine("Enter the number of elements in the array: ");
-            string s = Console.ReadLine();
-            n = Int32.Parse(s);
-            if((n > 0) && (n <= 20))
-               break;
-            else
-                Console.WriteLine("\nArray should have minimum 1 and maximum 20 elements. \n");
+            {
+                Console.WriteLine("Enter the number of elements in the array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 elements. \n");
+            }
+            //Accept array elements
+            Console.WriteLine("");
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Enter array elements");
+            Console.WriteLine("---------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
         }
-    }
-}
+
+        public void BinarySearch()
+        {
+
+        }
